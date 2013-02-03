45b230d36724694cab868ab4be91f1b4f8110aeb
Alexander Vogel <alexander.vogel@caegroup.de>
2013-02-03 17:06:09 +0100

    arbeit

--- src/main/java/de/caegroup/pradar/Db.java
+++ src/main/java/de/caegroup/pradar/Db.java
@@ -47,19 +47,106 @@ public class Db
 	----------------------------*/
 	public void initDb()
 	{
-		Connection connection = null;
-		Entity muster = new Entity();
-		Class entityClass = muster.getClass();
-		Field[] fields = entityClass.getFields();
-		for (int i=0; i < fields.length; i++)
+		try
 		{
-			System.out.println("Public field found: " + fields[i].toString());
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
+		Connection connection = null;
+		try
+		{
+			System.out.println("getting connection to: this.dbfile.getAbsolutePath()");
+			connection = DriverManager.getConnection("jdbc:sqlite:"+this.dbfile.getAbsolutePath());
+			
+			
+			Statement statement = connection.createStatement();
+			
+			statement.setQueryTimeout(10);
+			
+			statement.executeUpdate("drop table if exists radar");
+			statement.executeUpdate("create table radar (id UNIQUE, processname, host, user, checkin, checkout, restart)");
+			
+//			connection.commit();
+			connection.close();
+
+		} catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+// falls es automatisch von den feldern in entity abgeleitet werden soll
+//		Class entityClass = muster.getClass();
+//		Field[] fields = entityClass.getFields();
+//		for (int i=0; i < fields.length; i++)
+//		{
+//			System.out.println("Public field found: " + fields[i].toString());
+//		}
 	}
 
-	public void checkinEntity(Entity entity) throws ClassNotFoundException
+	public void checkinEntity(Entity entity)
 	{
-		Class.forName("org.sqlite.JDBC");
+		try
+		{
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Connection connection = null;
+		try
+		{
+			connection = DriverManager.getConnection("jdbc:sqlite:"+this.dbfile.getAbsolutePath());
+			Statement statement = connection.createStatement();
+			
+			statement.setQueryTimeout(30);
+			
+			statement.executeUpdate("INSERT INTO radar (id, processname, host, user, checkin, restart) VALUES ("+entity.getId()+", "+entity.getProcessname()+", "+entity.getHost()+", "+entity.getUser()+", "+entity.getCheckin()+", "+entity.isRestart()+")");
+			
+			connection.commit();
+			connection.close();
+		} catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void checkoutEntity(Entity entity)
+	{
+		try
+		{
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Connection connection = null;
+		try
+		{
+			connection = DriverManager.getConnection("jdbc:sqlite:"+this.dbfile.getAbsolutePath());
+			Statement statement = connection.createStatement();
+			
+			statement.setQueryTimeout(30);
+			
+			statement.executeUpdate("INSERT INTO radar (checkout) VALUES ("+entity.getCheckout()+") WHERE (id="+entity.getId()+")");
+			
+			connection.commit();
+			connection.close();
+		} catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	public void getEntityById(String id) throws ClassNotFoundException
