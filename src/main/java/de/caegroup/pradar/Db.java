eaaa38fbe7eb3751ac8411917bea92539c5e8d45
Alexander Vogel <alexander.vogel@caegroup.de>
2013-02-14 14:45:54 +0100

    arbeit

--- src/main/java/de/caegroup/pradar/Db.java
+++ src/main/java/de/caegroup/pradar/Db.java
@@ -66,7 +66,7 @@ public class Db
 	----------------------------*/
 	public void initDb()
 	{
-		System.out.println("getting connection to: "+this.dbfile.getAbsolutePath());
+		System.out.println("2: getting connection to: "+this.dbfile.getAbsolutePath());
 		try
 		{
 			Class.forName("org.sqlite.JDBC");
