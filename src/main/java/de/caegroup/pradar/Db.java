0d6c80c2d5d3c5acc7c0b06ef450c4322bb19428
Alexander Vogel <alexander.vogel@caegroup.de>
2013-02-15 17:32:56 +0100

    arbeit

--- src/main/java/de/caegroup/pradar/Db.java
+++ src/main/java/de/caegroup/pradar/Db.java
@@ -135,7 +135,7 @@ public class Db
 			System.out.println(sql);
 			ResultSet rs = statement.executeQuery(sql);
 			
-			String formatstring = "| %11s | %11s | %7s | %13s | %5s | %22s | %22s |\n";
+			String formatstring = "|%11s|%11s|%7s|%13s|%6s|%22s|%22s|\n";
 			System.out.format(formatstring, "id", "process", "user", "host", "active", "checkin", "checkout");
 			long Value = new Long("55");
 			while (rs.next())
