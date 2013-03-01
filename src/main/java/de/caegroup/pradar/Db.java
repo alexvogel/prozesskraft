8d14aaa3dce72c784a54ea809d18234b5d23a24e
Alexander Vogel <alexander.vogel@caegroup.de>
2013-03-01 16:05:49 +0100

    spaltenbreite 'id' 11->14

--- src/main/java/de/caegroup/pradar/Db.java
+++ src/main/java/de/caegroup/pradar/Db.java
@@ -179,7 +179,7 @@ public class Db
 				System.out.println("result is empty!");
 			}
 			
-			String formatstring = "|%-11s|%-11s|%-7s|%-13s|%-6s|%-23s|%-23s|%-8s|\n";
+			String formatstring = "|%-14s|%-11s|%-7s|%-13s|%-6s|%-23s|%-23s|%-8s|\n";
 			System.out.format(formatstring, "id", "process", "user", "host", "active", "checkin", "checkout", "exitcode");
 			System.out.format(formatstring, "-----------", "-----------", "-------", "-------------", "------", "-----------------------", "-----------------------", "--------");
 
