6595420cdc7ef8b9d628145c95f5512eef1c6eb6
Alexander Vogel <alexander.vogel@caegroup.de>
2013-03-18 20:05:01 +0100

    deploy.pl holt jetzt die fertig zusammengestellte jar aus dem targetdirectory

--- deploy_gtk.linux.x86_64.pl
+++ deploy_gtk.linux.x86_64.pl
@@ -20,9 +20,14 @@ system "mvn install -P gtk.linux.x86_64";
 # nachfolgern bescheid geben wie das targetverzeichnis heisst
 #system "echo 'java -jar manager-0.1.jar $@' > /tmp/pradar/bin/pradar-core";
 
-# nachfolgern bescheid geben wie das targetverzeichnis heisst
+# erstellen eines auslieferungsverzeichnisses und einkopieren des fertigen jars mit allem
 my $cwd = getcwd;
-my $auslieferungsverzeichnis = $cwd . "/target/fertig_zur_installation";
+my $target = $cwd . "/target";
+my $auslieferungsverzeichnis = $target . "/fertig_zur_installation";
 
+# kopieren des fertig zusammengestellten jars vom target ins auslieferungsverzeichnis
+system "mv $target/*jar-with-dependencies* $auslieferungsverzeichnis";
+
+# nachfolgern bescheid geben wie das targetverzeichnis heisst
 print "<newdir>$auslieferungsverzeichnis<newdir>\n";
 
