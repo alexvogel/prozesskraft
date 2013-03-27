fef5b6a69a5e03c8e34dc08c87dfce3b6439dc22
Alexander Vogel <alexander.vogel@caegroup.de>
2013-03-27 14:05:05 +0100

    arbeit

--- src/main/java/de/caegroup/pradar/parts/PradarViewProcessingEntity.java
+++ src/main/java/de/caegroup/pradar/parts/PradarViewProcessingEntity.java
@@ -213,11 +213,9 @@ public class PradarViewProcessingEntity
 		System.out.println("distance nach nachbehandlung: "+distance);
 		// antigravitation nimmt mit dem quadrat des abstands ab
 		
-		if ( Math.abs(distance) < 3.14)
-		{
-			antigravitypuls = (distance / (Math.pow((10*distance),2))) * this.gravity;
+		antigravitypuls = (distance / (Math.pow((10*distance),3))) * this.gravity;
 //			float antigravitypulsMap = PApplet.map((float)antigravitypuls, (float)-10, (float)10, (float)-0.01, (float)0.01);
-		}
+
 		System.out.println("antigravitypuls: "+antigravitypuls);
 		return antigravitypuls;
 
