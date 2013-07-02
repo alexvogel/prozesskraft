d0cafec5cf0e2930ecf01f3c5e29efd98991db83
Alexander Vogel <alexander.vogel@caegroup.de>
2013-07-02 17:42:14 +0200

    java version zeigt jetzt immer auf 'latest'

--- bin/license4j-server-admin.sh
+++ bin/license4j-server-admin.sh
@@ -10,7 +10,8 @@
 # SET YOUR JAVA PATH HERE
 #
 #JAVA=/usr/java/jdk1.7.0_07
-JAVA=<ersetze_mich_durch_java_home>
+#JAVA=<ersetze_mich_durch_java_home>
+JAVA=/usr/java/latest
 #
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
