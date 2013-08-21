318d22eead116ab7c0b77cf7f7fb2b3481315ca3
Alexander Vogel <alexander.vogel@caegroup.de>
2013-08-21 19:31:52 +0200

    modifikation deployment

--- deploy_gtk.linux.x86.pl
+++ deploy_gtk.linux.x86.pl
@@ -25,13 +25,32 @@ my $auslieferungsverzeichnis = $cwd . "/fertig_zur_installation";
 # kopieren des fertig zusammengestellten jars vom target ins auslieferungsverzeichnis
 print "mkdir $auslieferungsverzeichnis\n";
 system "mkdir $auslieferungsverzeichnis";
-print "mkdir $auslieferungsverzeichnis/bin\n";
-system "mkdir $auslieferungsverzeichnis/bin";
-print "cp -r $target/*jar-with-dependencies* $auslieferungsverzeichnis/bin/.\n";
-system "cp -r $target/*jar-with-dependencies* $auslieferungsverzeichnis/bin/.";
+print "cp -r $cwd/bin $auslieferungsverzeichnis/.\n";
+system "cp -r $cwd/bin $auslieferungsverzeichnis/.";
+print "cp -r $target/*with-my-dep* $auslieferungsverzeichnis/bin/.\n";
+system "cp -r $target/*with-my-dep* $auslieferungsverzeichnis/bin/.";
+print "cp -r $target/lib $auslieferungsverzeichnis/bin/.\n";
+system "cp -r $target/lib $auslieferungsverzeichnis/bin/.";
+
 
 # kopieren von resourcen ausserhalb des jars ins auslieferungsverzeichnis
+print "cp -r $cwd/etc $auslieferungsverzeichnis/.\n";
 system "cp -r $cwd/etc $auslieferungsverzeichnis/.";
+
+print "cp -r $target/lib $auslieferungsverzeichnis/.\n";
+system "cp -r $target/lib $auslieferungsverzeichnis/.";
+
+# erstellen von 32Bit und 64Bit Verzeichnissen
+#print "mkdir $auslieferungsverzeichnis/lib32\n";
+#system "mkdir $auslieferungsverzeichnis/lib32";
+#print "mkdir $auslieferungsverzeichnis/lib64\n";
+#system "mkdir $auslieferungsverzeichnis/lib64";
+
+# verschieben aller Bit-Abhaengigen Abhaengigkeiten in die entsprechenden ordner
+#print "mv $auslieferungsverzeichnis/lib/*x86_64* $auslieferungsverzeichnis/lib64\n";
+#system "mv $auslieferungsverzeichnis/lib/*x86_64* $auslieferungsverzeichnis/lib64";
+#print "mv $auslieferungsverzeichnis/lib/*x86* $auslieferungsverzeichnis/lib32\n";
+#system "mv $auslieferungsverzeichnis/lib/*x86* $auslieferungsverzeichnis/lib32";
 
 # nachfolgern bescheid geben wie das auslieferungsverzeichnis heisst
 print "<newdir>$auslieferungsverzeichnis<newdir>\n";
