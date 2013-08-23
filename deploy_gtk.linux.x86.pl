d3ed2d2d119bceb8f73ade3a9f9037301dac736f
Alexander Vogel <alexander.vogel@caegroup.de>
2013-08-23 18:37:51 +0200

    update deployscripte

--- deploy_gtk.linux.x86.pl
+++ deploy_gtk.linux.x86.pl
@@ -8,8 +8,8 @@ $|=1;
 
 # tmp-target-directory loeschen
 print "loeschen des targetverzeichnisses\n";
-print "rm -rf /tmp/pradar\n";
-system "rm -rf /tmp/pradar";
+print "rm -rf /tmp/pramp\n";
+system "rm -rf /tmp/pramp";
 
 # maven ausfuehren
 print "**\n";
@@ -25,13 +25,30 @@ my $auslieferungsverzeichnis = $cwd . "/fertig_zur_installation";
 # kopieren des fertig zusammengestellten jars vom target ins auslieferungsverzeichnis
 print "mkdir $auslieferungsverzeichnis\n";
 system "mkdir $auslieferungsverzeichnis";
-print "mkdir $auslieferungsverzeichnis/bin\n";
-system "mkdir $auslieferungsverzeichnis/bin";
-print "cp -r $target/*jar-with-dependencies* $auslieferungsverzeichnis/bin/.\n";
-system "cp -r $target/*jar-with-dependencies* $auslieferungsverzeichnis/bin/.";
+print "cp -r $cwd/bin $auslieferungsverzeichnis/.\n";
+system "cp -r $cwd/bin $auslieferungsverzeichnis/.";
+print "cp -r $target/*[0123456789].jar $auslieferungsverzeichnis/bin/.\n";
+system "cp -r $target/*[0123456789].jar $auslieferungsverzeichnis/bin/.";
+
 
 # kopieren von resourcen ausserhalb des jars ins auslieferungsverzeichnis
+print "cp -r $cwd/etc $auslieferungsverzeichnis/.\n";
 system "cp -r $cwd/etc $auslieferungsverzeichnis/.";
+
+print "cp -r $target/lib $auslieferungsverzeichnis/.\n";
+system "cp -r $target/lib $auslieferungsverzeichnis/.";
+
+# erstellen von 32Bit und 64Bit Verzeichnissen
+#print "mkdir $auslieferungsverzeichnis/lib32\n";
+#system "mkdir $auslieferungsverzeichnis/lib32";
+#print "mkdir $auslieferungsverzeichnis/lib64\n";
+#system "mkdir $auslieferungsverzeichnis/lib64";
+
+# verschieben aller Bit-Abhaengigen Abhaengigkeiten in die entsprechenden ordner
+#print "mv $auslieferungsverzeichnis/lib/*x86_64* $auslieferungsverzeichnis/lib64\n";
+#system "mv $auslieferungsverzeichnis/lib/*x86_64* $auslieferungsverzeichnis/lib64";
+#print "mv $auslieferungsverzeichnis/lib/*x86* $auslieferungsverzeichnis/lib32\n";
+#system "mv $auslieferungsverzeichnis/lib/*x86* $auslieferungsverzeichnis/lib32";
 
 # nachfolgern bescheid geben wie das auslieferungsverzeichnis heisst
 print "<newdir>$auslieferungsverzeichnis<newdir>\n";
