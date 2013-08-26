4ee7a7cb830aab28eb697b8eaceb5eb1aefb6424
Alexander Vogel <alexander.vogel@caegroup.de>
2013-08-26 16:27:42 +0200

    deployment umgestellt auf unabhaengig von bittichkeit

--- deploy_gtk.linux.x86.pl
+++ deploy_gtk.linux.x86.pl
@@ -27,6 +27,10 @@ print "mkdir $auslieferungsverzeichnis\n";
 system "mkdir $auslieferungsverzeichnis";
 print "cp -r $cwd/bin $auslieferungsverzeichnis/.\n";
 system "cp -r $cwd/bin $auslieferungsverzeichnis/.";
+print "cp -r $cwd/lib32 $auslieferungsverzeichnis/.\n";
+system "cp -r $cwd/lib32 $auslieferungsverzeichnis/.";
+print "cp -r $cwd/lib64 $auslieferungsverzeichnis/.\n";
+system "cp -r $cwd/lib64 $auslieferungsverzeichnis/.";
 print "cp -r $target/*[0123456789].jar $auslieferungsverzeichnis/bin/.\n";
 system "cp -r $target/*[0123456789].jar $auslieferungsverzeichnis/bin/.";
 
@@ -35,20 +39,13 @@ system "cp -r $target/*[0123456789].jar $auslieferungsverzeichnis/bin/.";
 print "cp -r $cwd/etc $auslieferungsverzeichnis/.\n";
 system "cp -r $cwd/etc $auslieferungsverzeichnis/.";
 
+# loeschen von 32-Bit/64-Bit Jars aus dem allgemeinen lib-Verzeichnis
+print "rm -rf $target/lib/gtk.linux.x86*\n";
+system "rm -rf $target/lib/gtk.linux.x86*";
+
+# kopieren der allgemeinen lib ins auslieferungsverzeichnis
 print "cp -r $target/lib $auslieferungsverzeichnis/.\n";
 system "cp -r $target/lib $auslieferungsverzeichnis/.";
-
-# erstellen von 32Bit und 64Bit Verzeichnissen
-#print "mkdir $auslieferungsverzeichnis/lib32\n";
-#system "mkdir $auslieferungsverzeichnis/lib32";
-#print "mkdir $auslieferungsverzeichnis/lib64\n";
-#system "mkdir $auslieferungsverzeichnis/lib64";
-
-# verschieben aller Bit-Abhaengigen Abhaengigkeiten in die entsprechenden ordner
-#print "mv $auslieferungsverzeichnis/lib/*x86_64* $auslieferungsverzeichnis/lib64\n";
-#system "mv $auslieferungsverzeichnis/lib/*x86_64* $auslieferungsverzeichnis/lib64";
-#print "mv $auslieferungsverzeichnis/lib/*x86* $auslieferungsverzeichnis/lib32\n";
-#system "mv $auslieferungsverzeichnis/lib/*x86* $auslieferungsverzeichnis/lib32";
 
 # nachfolgern bescheid geben wie das auslieferungsverzeichnis heisst
 print "<newdir>$auslieferungsverzeichnis<newdir>\n";
