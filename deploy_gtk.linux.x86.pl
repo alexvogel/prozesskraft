142cee6aedcc18064cc5869bb4dcdb54d9c3e80a
Alexander Vogel <alexander.vogel@caegroup.de>
2013-08-26 16:12:50 +0200

    bit-abhaengige jars werden aus dem allgemeinen lib-verzeichnis geloescht

--- deploy_gtk.linux.x86.pl
+++ deploy_gtk.linux.x86.pl
@@ -42,6 +42,10 @@ system "cp -r $cwd/etc $auslieferungsverzeichnis/.";
 print "cp -r $target/lib $auslieferungsverzeichnis/.\n";
 system "cp -r $target/lib $auslieferungsverzeichnis/.";
 
+# loeschen von 32-Bit/64-Bit Jars aus dem allgemeinen lib-Verzeichnis
+print "rm -rf $target/lib/gtk.linux.x86*\n";
+system "rm -rf $target/lib/gtk.linux.x86*";
+
 # erstellen von 32Bit und 64Bit Verzeichnissen
 #print "mkdir $auslieferungsverzeichnis/lib32\n";
 #system "mkdir $auslieferungsverzeichnis/lib32";
