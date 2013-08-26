d0fb9ac4a37c0f028f7201918f982b94f241f0b2
Alexander Vogel <alexander.vogel@caegroup.de>
2013-08-26 15:30:38 +0200

    startscript ermittelt die bittigkeit von java und bindet das entsprechende lib-verzeichnis ein

--- bin/start.sh
+++ bin/start.sh
@@ -5,7 +5,29 @@ BASEDIR=$(dirname $0)
 # wenn eine lokale javaversion vorhanden ist, soll diese bevorzugt verwendet werden
 
 if [ -f $BASEDIR/../../../java/latest/bin/java ];
-	then $BASEDIR/../../../java/latest/bin/java -Djava.ext.dirs=$BASEDIR/../lib/ -jar $BASEDIR/*.jar
-	else java -Djava.ext.dirs=$BASEDIR/../lib/ -jar $BASEDIR/*.jar
+	then
+		echo using local java-installation
+		AUSGABE=$(($BASEDIR/../../../java/latest/bin/java -d32) 2>&1)
+		ERRORSTRING=$(echo $AUSGABE | grep -P '^Error' -o)
+		if [ "$ERRORSTRING" == "Error" ];
+			then
+				echo its a 64-Bit java
+				$BASEDIR/../../../java/latest/bin/java -Djava.ext.dirs=$BASEDIR/../lib/:$BASEDIR/../lib64/ -jar $BASEDIR/*.jar
+			else
+				echo its a 32-Bit java
+				$BASEDIR/../../../java/latest/bin/java -Djava.ext.dirs=$BASEDIR/../lib/:$BASEDIR/../lib32/ -jar $BASEDIR/*.jar
+		fi
+	else
+		echo using central java-installation
+		AUSGABE=$((java -d32) 2>&1)
+		ERRORSTRING=$(echo $AUSGABE | grep -P '^Error' -o)
+		if [ "$ERRORSTRING" == "Error" ];
+			then
+				echo its a 64-Bit java
+				java -Djava.ext.dirs=$BASEDIR/../lib/:$BASEDIR/../lib64/ -jar $BASEDIR/*.jar
+			else
+				echo its a 32-Bit java
+				java -Djava.ext.dirs=$BASEDIR/../lib/:$BASEDIR/../lib32/ -jar $BASEDIR/*.jar
+		fi
+		
 fi
-exit 0
