ce21effb1e5b48856d292b4fa0c3191daa844b2b
Alexander Vogel <alexander.vogel@caegroup.de>
2013-08-27 17:47:21 +0200

    update startscript

--- bin/start.sh
+++ bin/start.sh
@@ -13,10 +13,10 @@ if [ -f $BASEDIR/../../../java/latest/bin/java ];
 		if [ "$ERRORSTRING" == "Error" ];
 			then
 				echo its a 64-Bit java
-				$BASEDIR/../../../java/latest/bin/java -cp "$BASEDIR/../lib/*:$BASEDIR/../lib64/*:pradar-gui-0.4.0.jar" $MAINCLASS
+				$BASEDIR/../../../java/latest/bin/java -cp "$BASEDIR/../lib/*:$BASEDIR/../lib64/*:$BASEDIR/*" $MAINCLASS
 			else
 				echo its a 32-Bit java
-				$BASEDIR/../../../java/latest/bin/java -cp "$BASEDIR/../lib/*:$BASEDIR/../lib32/*:pradar-gui-0.4.0.jar" $MAINCLASS
+				$BASEDIR/../../../java/latest/bin/java -cp "$BASEDIR/../lib/*:$BASEDIR/../lib32/*:$BASEDIR/*" $MAINCLASS
 		fi
 	else
 		echo using central java-installation
@@ -25,10 +25,10 @@ if [ -f $BASEDIR/../../../java/latest/bin/java ];
 		if [ "$ERRORSTRING" == "Error" ];
 			then
 				echo its a 64-Bit java
-				java -cp "$BASEDIR/../lib/*:$BASEDIR/../lib64/*:pradar-gui-0.4.0.jar" $MAINCLASS
+				java -cp "$BASEDIR/../lib/*:$BASEDIR/../lib64/*:$BASEDIR/*" $MAINCLASS
 			else
 				echo its a 32-Bit java
-				java -cp "$BASEDIR/../lib/*:$BASEDIR/../lib32/*:pradar-gui-0.4.0.jar" $MAINCLASS
+				java -cp "$BASEDIR/../lib/*:$BASEDIR/../lib32/*:$BASEDIR/*" $MAINCLASS
 		fi
 		
 fi
