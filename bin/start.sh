ea5621c30db5ed6655b4da79921d17ee3d435d2c
Alexander Vogel <alexander.vogel@caegroup.de>
2013-08-28 18:59:03 +0200

    update startscript

--- bin/start.sh
+++ bin/start.sh
@@ -7,27 +7,27 @@ BASEDIR=$(dirname $0)
 
 if [ -f $BASEDIR/../../../java/latest/bin/java ];
 	then
-		echo using local java-installation
+		
 		AUSGABE=$(($BASEDIR/../../../java/latest/bin/java -d32) 2>&1)
 		ERRORSTRING=$(echo $AUSGABE | grep -P '^Error' -o)
 		if [ "$ERRORSTRING" == "Error" ];
 			then
-				echo its a 64-Bit java
+				echo using local java (64-bit)
 				$BASEDIR/../../../java/latest/bin/java -cp "$BASEDIR/../lib/*:$BASEDIR/../lib64/*:$BASEDIR/*" $MAINCLASS $@
 			else
-				echo its a 32-Bit java
+				echo using local java (32-bit)
 				$BASEDIR/../../../java/latest/bin/java -cp "$BASEDIR/../lib/*:$BASEDIR/../lib32/*:$BASEDIR/*" $MAINCLASS $@
 		fi
 	else
-		echo using central java-installation
+		
 		AUSGABE=$((java -d32) 2>&1)
 		ERRORSTRING=$(echo $AUSGABE | grep -P '^Error' -o)
 		if [ "$ERRORSTRING" == "Error" ];
 			then
-				echo its a 64-Bit java
+				echo using central java (64-bit)
 				java -cp "$BASEDIR/../lib/*:$BASEDIR/../lib64/*:$BASEDIR/*" $MAINCLASS $@
 			else
-				echo its a 32-Bit java
+				echo using central java (32-bit)
 				java -cp "$BASEDIR/../lib/*:$BASEDIR/../lib32/*:$BASEDIR/*" $MAINCLASS $@
 		fi
 		
