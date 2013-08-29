f94c6ed968a8bf1b1dd2a0cb7d3ad2a7f0ca534e
Alexander Vogel <alexander.vogel@caegroup.de>
2013-08-29 19:16:17 +0200

    update pom, build

---
+++ bin/start.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+MAINCLASS="de.caegroup.pradar.Progress"
+BASEDIR=$(dirname $0)
+
+# wenn eine lokale javaversion vorhanden ist, soll diese bevorzugt verwendet werden
+
+if [ -f $BASEDIR/../../../java/latest/bin/java ];
+	then
+		
+		AUSGABE=$(($BASEDIR/../../../java/latest/bin/java -d32) 2>&1)
+		ERRORSTRING=$(echo $AUSGABE | grep -P '^Error' -o)
+		if [ "$ERRORSTRING" == "Error" ];
+			then
+				echo using local java \(64-bit\)
+				$BASEDIR/../../../java/latest/bin/java -cp "$BASEDIR/../lib/*:$BASEDIR/../lib64/*:$BASEDIR/*" $MAINCLASS $@
+			else
+				echo using local java \(32-bit\)
+				$BASEDIR/../../../java/latest/bin/java -cp "$BASEDIR/../lib/*:$BASEDIR/../lib32/*:$BASEDIR/*" $MAINCLASS $@
+		fi
+	else
+		
+		AUSGABE=$((java -d32) 2>&1)
+		ERRORSTRING=$(echo $AUSGABE | grep -P '^Error' -o)
+		if [ "$ERRORSTRING" == "Error" ];
+			then
+				echo using central java \(64-bit\)
+				java -cp "$BASEDIR/../lib/*:$BASEDIR/../lib64/*:$BASEDIR/*" $MAINCLASS $@
+			else
+				echo using central java \(32-bit\)
+				java -cp "$BASEDIR/../lib/*:$BASEDIR/../lib32/*:$BASEDIR/*" $MAINCLASS $@
+		fi
+		
+fi
