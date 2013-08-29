9ba093abfefa2b3addb2716c3796c1cc2388bc55
Alexander Vogel <alexander.vogel@caegroup.de>
2013-08-29 18:59:02 +0200

    update pom, build

--- deploy.pl
+++ deploy.pl
@@ -20,12 +20,22 @@ system "mvn install";
 # erstellen eines auslieferungsverzeichnisses und einkopieren des fertigen jars mit allem
 my $cwd = getcwd;
 my $target = $cwd . "/target";
-my $auslieferungsverzeichnis = $target . "/fertig_zur_installation";
+my $auslieferungsverzeichnis = $cwd . "/fertig_zur_installation";
 
 # kopieren des fertig zusammengestellten jars vom target ins auslieferungsverzeichnis
+print "mkdir $auslieferungsverzeichnis\n";
 system "mkdir $auslieferungsverzeichnis";
-system "mkdir $auslieferungsverzeichnis/bin";
-system "mv $target/*jar-with-dependencies* $auslieferungsverzeichnis/bin";
+print "cp -r $cwd/bin $auslieferungsverzeichnis/.\n";
+system "cp -r $cwd/bin $auslieferungsverzeichnis/.";
+print "cp -r $target/*[0123456789].jar $auslieferungsverzeichnis/bin/.\n";
+system "cp -r $target/*[0123456789].jar $auslieferungsverzeichnis/bin/.";
 
-# nachfolgern bescheid geben wie das targetverzeichnis heisst
+# kopieren von resourcen ausserhalb des jars ins auslieferungsverzeichnis
+system "cp -r $cwd/etc $auslieferungsverzeichnis/.";
+
+# kopieren der allgemeinen lib ins auslieferungsverzeichnis
+print "cp -r $target/lib $auslieferungsverzeichnis/.\n";
+system "cp -r $target/lib $auslieferungsverzeichnis/.";
+
+# nachfolgern bescheid geben wie das auslieferungsverzeichnis heisst
 print "<newdir>$auslieferungsverzeichnis<newdir>\n";
