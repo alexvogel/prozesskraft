d764ca63064d27a1f853f0025aa27acaced82eaf
Alexander Vogel <alexander.vogel@caegroup.de>
2013-08-29 19:23:44 +0200

    update pom, build

--- deploy.pl
+++ deploy.pl
@@ -8,8 +8,8 @@ $|=1;
 
 # tmp-target-directory loeschen
 print "loeschen des targetverzeichnisses\n";
-print "rm -rf /tmp/pradar\n";
-system "rm -rf /tmp/pradar";
+#print "rm -rf /tmp/pradar\n";
+#system "rm -rf /tmp/pradar";
 
 # maven ausfuehren
 print "**\n";
@@ -25,13 +25,17 @@ my $auslieferungsverzeichnis = $cwd . "/fertig_zur_installation";
 # kopieren des fertig zusammengestellten jars vom target ins auslieferungsverzeichnis
 print "mkdir $auslieferungsverzeichnis\n";
 system "mkdir $auslieferungsverzeichnis";
-print "mkdir $auslieferungsverzeichnis/bin\n";
-system "mkdir $auslieferungsverzeichnis/bin";
-print "cp -r $target/*jar-with-dependencies* $auslieferungsverzeichnis/bin/.\n";
-system "cp -r $target/*jar-with-dependencies* $auslieferungsverzeichnis/bin/.";
+print "cp -r $cwd/bin $auslieferungsverzeichnis/.\n";
+system "cp -r $cwd/bin $auslieferungsverzeichnis/.";
+print "cp -r $target/*[0123456789].jar $auslieferungsverzeichnis/bin/.\n";
+system "cp -r $target/*[0123456789].jar $auslieferungsverzeichnis/bin/.";
 
 # kopieren von resourcen ausserhalb des jars ins auslieferungsverzeichnis
 system "cp -r $cwd/etc $auslieferungsverzeichnis/.";
+
+# kopieren der allgemeinen lib ins auslieferungsverzeichnis
+print "cp -r $target/lib $auslieferungsverzeichnis/.\n";
+system "cp -r $target/lib $auslieferungsverzeichnis/.";
 
 # nachfolgern bescheid geben wie das auslieferungsverzeichnis heisst
 print "<newdir>$auslieferungsverzeichnis<newdir>\n";
