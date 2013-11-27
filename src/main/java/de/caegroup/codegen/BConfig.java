65311af01efcfd87248b712b971079debc8f62e5
Alexander Vogel <alexander.vogel@caegroup.de>
2013-11-27 17:25:39 +0100

    perlcode fehlerkor

--- src/main/java/de/caegroup/codegen/BConfig.java
+++ src/main/java/de/caegroup/codegen/BConfig.java
@@ -47,13 +47,13 @@ implements Serializable, Cloneable
 	{
 		ArrayList<String> content = new ArrayList<String>();
 
-		content.add("my %CONF_ORG = &getvars($conf_path);\n");
-		content.add("my %CONF;\n");
-		content.add("# viele parameter im parameterfile enthalten pfade relativ zum installationsverzeichnis\n");
-		content.add("# diese pfade sollen auf absolute pfade expandiert werden\n");
-		content.add("logit(\"info\", \"expanding config parameter to absolute path\");\n");
-		content.add("foreach my $param (sort keys %CONF_ORG)\n");
-		content.add("{\n");
+		content.add("my %CONF_ORG = &getvars($conf_path);");
+		content.add("my %CONF;");
+		content.add("# viele parameter im parameterfile enthalten pfade relativ zum installationsverzeichnis");
+		content.add("# diese pfade sollen auf absolute pfade expandiert werden");
+		content.add("logit(\"info\", \"expanding config parameter to absolute path\");");
+		content.add("foreach my $param (sort keys %CONF_ORG)");
+		content.add("{");
 		content.add("# gibts da ein file? Ja? Dann soll auf den absoluten Pfad expandiert werden");
 		content.add("	if ((($CONF_ORG{$param} ne \"\") && (stat $bindir.\"/\".$CONF_ORG{$param})))");
 		content.add("	{");
--- src/main/java/de/caegroup/codegen/BOptions.java
+++ src/main/java/de/caegroup/codegen/BOptions.java
@@ -48,70 +48,70 @@ implements Serializable, Cloneable
 	{
 		ArrayList<String> content = new ArrayList<String>();
 
-		content.add("my %OPT;\n");
-		content.add("my %OPTHELP;\n");
-		content.add("my %optionsall;\n");
+		content.add("my %OPT;");
+		content.add("my %OPTHELP;");
+		content.add("my %optionsall;");
 
-		content.add("# defaultoptions\n");
-		content.add("# hier koenne alle 'festen' optionen definiert werden\n");
-		content.add("my %options_fest = (\n");
+		content.add("# defaultoptions");
+		content.add("# hier koenne alle 'festen' optionen definiert werden");
+		content.add("my %options_fest = (");
 		
-		content.add("\t\t\t\t'help' => {'minoccur' => '0', 'maxoccur' => '1', 'definition' => 'help|h', 'check'=>'', 'default' => undef, 'text1' => '', 'text2' => 'gibt diesen hilfetext aus'},\n");
-		content.add("\t\t\t\t'doc'  => {'minoccur' => '0', 'maxoccur' => '1', 'definition' => 'doc', 'check'=>'', 'default' => undef, 'text1' => '', 'text2' => 'falls eine dokumentation existiert, wird diese angezeigt'},\n");
-		content.add("\t\t\t\t'log'  => {'minoccur' => '0', 'maxoccur' => '1', 'definition' => 'log=s', 'check'=>'', 'default' => undef, 'text1' => '=PATH', 'text2' => 'statt auf STDERR koennen logging-ausgaben in eine datei umgeleitet werden'},\n");
+		content.add("\t\t\t\t'help' => {'minoccur' => '0', 'maxoccur' => '1', 'definition' => 'help|h', 'check'=>'', 'default' => undef, 'text1' => '', 'text2' => 'gibt diesen hilfetext aus'},");
+		content.add("\t\t\t\t'doc'  => {'minoccur' => '0', 'maxoccur' => '1', 'definition' => 'doc', 'check'=>'', 'default' => undef, 'text1' => '', 'text2' => 'falls eine dokumentation existiert, wird diese angezeigt'},");
+		content.add("\t\t\t\t'log'  => {'minoccur' => '0', 'maxoccur' => '1', 'definition' => 'log=s', 'check'=>'', 'default' => undef, 'text1' => '=PATH', 'text2' => 'statt auf STDERR koennen logging-ausgaben in eine datei umgeleitet werden'},");
 		
 		for(Option o: option)
 		{
 			content.addAll(o.getContent());
 		}
 		
-		content.add(");\n");
+		content.add(");");
 
-		content.add("# parsen und generieren eines hashs (%optionsall) zum erzeugen von optionen (Getopt::Long)\n");
-		content.add("foreach my $optionname (keys %options_fest)\n");
-		content.add("{\n");
+		content.add("# parsen und generieren eines hashs (%optionsall) zum erzeugen von optionen (Getopt::Long)");
+		content.add("foreach my $optionname (keys %options_fest)");
+		content.add("{");
 
 		
-		content.add("\tif ( ${$options_fest{$optionname}}{'maxoccur'} > 1 )\n");
-		content.add("\t{\n");
-		content.add("\t\tmy @tmp;\n");
-		content.add("\t\t$optionsall{${$options_fest{$optionname}}{'definition'}} = \\@tmp;\n");
-		content.add("\n");
+		content.add("\tif ( ${$options_fest{$optionname}}{'maxoccur'} > 1 )");
+		content.add("\t{");
+		content.add("\t\tmy @tmp;");
+		content.add("\t\t$optionsall{${$options_fest{$optionname}}{'definition'}} = \\@tmp;");
+		content.add("");
 			
-		content.add("\t\t$OPT{$optionname} = \\@tmp;\n");
-		content.add("\t\t$OPTHELP{$optionname} = {'text1' => ${$options_fest{$optionname}}{'text1'}, 'text2' => ${$options_fest{$optionname}}{'text2'}};\n");
-		content.add("\t}\n");
-		content.add("\telse\n");
-		content.add("\t{\n");
-		content.add("\t\tmy $tmp;\n");
-		content.add("\t\t$optionsall{${$options_fest{$optionname}}{'definition'}} = \\$tmp;\n");
-		content.add("\n");
+		content.add("\t\t$OPT{$optionname} = \\@tmp;");
+		content.add("\t\t$OPTHELP{$optionname} = {'text1' => ${$options_fest{$optionname}}{'text1'}, 'text2' => ${$options_fest{$optionname}}{'text2'}};");
+		content.add("\t}");
+		content.add("\telse");
+		content.add("\t{");
+		content.add("\t\tmy $tmp;");
+		content.add("\t\t$optionsall{${$options_fest{$optionname}}{'definition'}} = \\$tmp;");
+		content.add("");
 
-		content.add("\t\t$OPT{$optionname} = \\$tmp;\n");
-		content.add("\t\t$OPTHELP{$optionname} = {'text1' => ${$options_fest{$optionname}}{'text1'}, 'text2' => ${$options_fest{$optionname}}{'text2'}};\n");
-		content.add("\t}\n");
-		content.add("}\n");
+		content.add("\t\t$OPT{$optionname} = \\$tmp;");
+		content.add("\t\t$OPTHELP{$optionname} = {'text1' => ${$options_fest{$optionname}}{'text1'}, 'text2' => ${$options_fest{$optionname}}{'text2'}};");
+		content.add("\t}");
+		content.add("}");
 
-		content.add("# das tatsaechliche erzeugen der options mit Getopt::Long\n");
-		content.add("my $result = GetOptions(%optionsall);\n");
+		content.add("# das tatsaechliche erzeugen der options mit Getopt::Long");
+		content.add("my $result = GetOptions(%optionsall);");
 
-		content.add("unless($result)\n");
-		content.add("{\n");
-		content.add("	logit('warn', 'problem occured while reading commandline options. information given with unknown options will be ignored. this could be a problem.');\n");
-		content.add("#	exit(1);\n");
-		content.add("}\n");
+		content.add("unless($result)");
+		content.add("{");
+		content.add("	logit('warn', 'problem occured while reading commandline options. information given with unknown options will be ignored. this could be a problem.');");
+		content.add("#	exit(1);");
+		content.add("}");
 
-		content.add("# OPTIONEN DEFAULTS ZUWEISEN\n");
-		content.add("foreach(keys %options_fest)\n");
-		content.add("{\n");
-		content.add("	# wenn eine option nicht vorhanden ist, aber dafuer ein default-wert existiert, soll dieser gesetzt werden\n");
-		content.add("	if (!(${$OPT{$_}}) && ${$options_fest{$_}}{'default'})\n");
-		content.add("#	if ( ${$options_fest{$_}}{'default'})\n");
-		content.add("	{\n");
-		content.add("		logit('info', 'setting default for --' . $_ . '=' . ${$options_fest{$_}}{'default'} );\n");
-		content.add("		${$OPT{$_}} = ${$options_fest{$_}}{'default'};\n");
-		content.add("	}\n");
-		content.add("}\n");
+		content.add("# OPTIONEN DEFAULTS ZUWEISEN");
+		content.add("foreach(keys %options_fest)");
+		content.add("{");
+		content.add("	# wenn eine option nicht vorhanden ist, aber dafuer ein default-wert existiert, soll dieser gesetzt werden");
+		content.add("	if (!(${$OPT{$_}}) && ${$options_fest{$_}}{'default'})");
+		content.add("#	if ( ${$options_fest{$_}}{'default'})");
+		content.add("	{");
+		content.add("		logit('info', 'setting default for --' . $_ . '=' . ${$options_fest{$_}}{'default'} );");
+		content.add("		${$OPT{$_}} = ${$options_fest{$_}}{'default'};");
+		content.add("	}");
+		content.add("}");
 
 		return content;
 	}
