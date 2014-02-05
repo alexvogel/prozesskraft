53f0f38e6a4cbdb62488bb059ce2e82db9854f3d
Alexander Vogel <alexander.vogel@caegroup.de>
2014-02-05 15:08:36 +0100

    debug

--- src/main/java/de/caegroup/process/Createdoc.java
+++ src/main/java/de/caegroup/process/Createdoc.java
@@ -416,7 +416,7 @@ public class Createdoc
 		
 		report.setParameter("processName", process.getName());
 		report.setParameter("processVersion", process.getModelVersion());
-		report.setParameter("processDatum", timestamp.toString());
+		report.setParameter("processDatum", "unbekannt");
 		report.setParameter("processArchitectCompany", process.getArchitectCompany());
 		report.setParameter("processArchitectName", process.getArchitectName());
 		report.setParameter("processArchitectMail", process.getArchitectMail());
