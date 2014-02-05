2cebcf54dbbfa9e47458df73af436c88f56c4050
Alexander Vogel <alexander.vogel@caegroup.de>
2014-02-05 15:14:36 +0100

    fehlerkorr

--- src/main/java/de/caegroup/process/Createdoc.java
+++ src/main/java/de/caegroup/process/Createdoc.java
@@ -416,7 +416,7 @@ public class Createdoc
 		
 		report.setParameter("processName", process.getName());
 		report.setParameter("processVersion", process.getModelVersion());
-		report.setParameter("processDate", timestamp.toString());
+		report.setParameter("processDatum", process.getDate());
 		report.setParameter("processArchitectCompany", process.getArchitectCompany());
 		report.setParameter("processArchitectName", process.getArchitectName());
 		report.setParameter("processArchitectMail", process.getArchitectMail());
