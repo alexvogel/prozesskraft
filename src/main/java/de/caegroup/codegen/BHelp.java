356c7f8d2ad585315d8fe56e97816d43cd1363ce
Alexander Vogel <alexander.vogel@caegroup.de>
2014-03-25 14:43:59 +0100

    korr

--- src/main/java/de/caegroup/codegen/BHelp.java
+++ src/main/java/de/caegroup/codegen/BHelp.java
@@ -100,7 +100,7 @@ implements Serializable, Cloneable
 
 			content.add("$helptext .= \"author: alexander.vogel\\@caegroup.de | version: $version | date: $date\\n\\n\";");
 			content.add("");
-			content.add("$helptext .= \"installdir: \" . Cwd->realpath(File::Spec->rel2abs($0)) . \"\\n\";");
+			content.add("$helptext .= \"installdir: \" . Cwd::realpath(File::Spec->rel2abs($0)) . \"\\n\";");
 		}
 		
 		this.block.setOrigin("auto");
