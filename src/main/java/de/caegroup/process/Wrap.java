c2e5c2a036050c6352f09099808a84eb3f49bfa3
Alexander Vogel <alexander.vogel@caegroup.de>
2014-04-07 14:58:57 +0200

    debug

--- src/main/java/de/caegroup/process/Wrap.java
+++ src/main/java/de/caegroup/process/Wrap.java
@@ -153,7 +153,7 @@ public class Wrap
 		p3.setOutfilexml(output.getAbsolutePath());
 
 		// den neuen wrap-process rausschreiben
-		p3.writeXml2();
+		p3.writeXml();
 	}
 	
 	private static void exiter()
