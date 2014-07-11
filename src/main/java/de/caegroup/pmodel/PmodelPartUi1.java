900bfc011558faaab20d9fdb6f807e029cc7429b
Alexander Vogel <alexander.vogel@caegroup.de>
2014-07-11 21:24:12 +0200

    korr

--- src/main/java/de/caegroup/pmodel/PmodelPartUi1.java
+++ src/main/java/de/caegroup/pmodel/PmodelPartUi1.java
@@ -1104,7 +1104,7 @@ public class PmodelPartUi1 extends ModelObject
 					Shell shell = new Shell(display);
 					shell.setText("pmodel-gui "+"v[% version %]");
 
-					shell.setImage(new Image(display, "logoSymbol50Transp.png"));
+					shell.setImage(new Image(display, this.getClass().getResourceAsStream("/logoSymbol50Transp.png")));
 					shell.setLayout(new FillLayout());
 					Composite composite = new Composite(shell, SWT.NO_FOCUS);
 					GridLayout gl_composite = new GridLayout(2, false);
