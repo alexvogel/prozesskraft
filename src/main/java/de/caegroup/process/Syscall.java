04223ce24fea7922edfae909138d94b3e5f5704e
avoge <alexander.vogel@caegroup.de>
2014-08-30 15:28:21 +0200

    aufruf wird jetzt mit benamten parametern erwartet

--- src/main/java/de/caegroup/process/Syscall.java
+++ src/main/java/de/caegroup/process/Syscall.java
@@ -2,58 +2,183 @@ package de.caegroup.process;
 
 import java.io.*;
 import java.lang.management.ManagementFactory;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
+import java.sql.Timestamp;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+
 public class Syscall {
 
-	/* Aufruf:
-	 * $1 Vollstaendiger Aufruf mit allen Parametern
-	 * $2 absoluter Pfad zum stdout-ausgabefile. Dieses wird neu angelegt und darin wird der ganze stdout von $1 geschrieben
-	 * $3 absoluter Pfad zum stderr-ausgabefile. Dieses wird neu angelegt und darin wird der ganze stderr von $1 geschrieben
-	*/
-	public static void main(String[] args) {
+	/*----------------------------
+	  structure
+	----------------------------*/
+	static CommandLine commandline;
+	static String author = "alexander.vogel@caegroup.de";
+	static String version = "[% version %]";
+	static String date = "[% date %]";
+
+	public static void main(String[] args) throws org.apache.commons.cli.ParseException, IOException
+	{
 	 
+		/*----------------------------
+		  create boolean options
+		----------------------------*/
+		Option ohelp = new Option("help", "print this message");
+		Option ov = new Option("v", "prints version and build-date");
+		
+		/*----------------------------
+		  create argument options
+		----------------------------*/
+		Option ocall = OptionBuilder.withArgName("STRING")
+				.hasArg()
+				.withDescription("[mandatory] the full call that should be executed. escape whitespaces or use \"\"")
+//				.isRequired()
+				.create("call");
+		
+		Option olog = OptionBuilder.withArgName("FILE")
+				.hasArg()
+				.withDescription("[mandatory] the executed call will be printed to this file.")
+//				.isRequired()
+				.create("log");
+		
+		Option ostdout = OptionBuilder.withArgName("FILE")
+				.hasArg()
+				.withDescription("[mandatory] the STDOUT of the executed call will be redirected into this file.")
+//				.isRequired()
+				.create("stdout");
+		
+		Option ostderr = OptionBuilder.withArgName("FILE")
+				.hasArg()
+				.withDescription("[mandatory] the STDERR of the executed call will be redirected into this file.")
+//				.isRequired()
+				.create("stderr");
+
+		Option opid = OptionBuilder.withArgName("FILE")
+				.hasArg()
+				.withDescription("[mandatory] the process-Id of the executed call will be written to this file.")
+//				.isRequired()
+				.create("pid");
+
+		
+		/*----------------------------
+		  create options object
+		----------------------------*/
+		Options options = new Options();
+		
+		options.addOption( ohelp );
+		options.addOption( ov );
+		options.addOption( ocall );
+		options.addOption( ostdout );
+		options.addOption( ostderr );
+		options.addOption( opid );
+		options.addOption( olog );
+		
+		/*----------------------------
+		  create the parser
+		----------------------------*/
+		CommandLineParser parser = new GnuParser();
 		try
 		{
-			if (args.length < 4)
-			{
-				System.out.println("Please specify arg1=systemcall arg-3=fullpath_stdout arg-2=fullpath_stderr arg-1=fullpath_pid");
-			}
+			// parse the command line arguments
+			commandline = parser.parse( options,  args );
 			
 		}
-		catch (ArrayIndexOutOfBoundsException e)
+		catch ( Exception exp )
 		{
-			System.out.println("***ArrayIndexOutOfBoundsException\n" + e.toString());
+			// oops, something went wrong
+			System.err.println( "Parsing failed. Reason: "+ exp.getMessage());
+			exiter();
 		}
-
-		// umwandeln aller parameter in eine liste
-		ArrayList<String> argumente = new ArrayList<String>(Arrays.asList(args));
 		
-		// umkopieren der stdout/stderr filepfade
-		String absdir_stdout = argumente.get(argumente.size()-3);
-		String absdir_stderr = argumente.get(argumente.size()-2);
-		String absdir_pid    = argumente.get(argumente.size()-1);
-
-		// entfernen der stdout/sdterr/pid filepfade
-		argumente.remove(argumente.size()-1);
-		argumente.remove(argumente.size()-1);
-		argumente.remove(argumente.size()-1);
+		/*----------------------------
+		  usage/help
+		----------------------------*/
+		if ( commandline.hasOption("help"))
+		{
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("wrap", options);
+			System.exit(0);
+		}
+		
+		else if ( commandline.hasOption("v"))
+		{
+			System.out.println("web:     www.prozesskraft.de");
+			System.out.println("version: [% version %]");
+			System.out.println("date:    [% date %]");
+			System.exit(0);
+		}
+		
+		/*----------------------------
+		  ueberpruefen ob eine schlechte kombination von parametern angegeben wurde
+		----------------------------*/
+		if ( !( commandline.hasOption("call")) )
+		{
+			System.err.println("option -call is mandatory.");
+			exiter();
+		}
+		
+		if ( !( commandline.hasOption("stdout")) )
+		{
+			System.err.println("option -stdout is mandatory.");
+			exiter();
+		}
+		
+		if ( !( commandline.hasOption("stderr")) )
+		{
+			System.err.println("option -stderr is mandatory.");
+			exiter();
+		}
+		
+		if ( !( commandline.hasOption("pid")) )
+		{
+			System.err.println("option -pid is mandatory.");
+			exiter();
+		}
+		
+		if ( !( commandline.hasOption("log")) )
+		{
+			System.err.println("option -log is mandatory.");
+			exiter();
+		}
 		
 		try {
 
-			String aufruf = "";
+			String sCall = commandline.getOptionValue("call");
+			String sLog = commandline.getOptionValue("log");
+			String sPid = commandline.getOptionValue("pid");
+			String sStdout = commandline.getOptionValue("stdout");
+			String sStderr = commandline.getOptionValue("stderr");
 
-			for(String actArgument : argumente)
-			{
-				aufruf = aufruf + " " + actArgument;
-			}
+			// DEBUG
+			
+			// Aufruf in das call -logfile schreiben
+			PrintWriter writerLog = new PrintWriter(sLog);
+			writerLog.println(new Timestamp(System.currentTimeMillis()));
+			writerLog.println("process syscall");
+			writerLog.println("-call \""+sCall+"\"");
+			writerLog.println("-stdout "+sStdout);
+			writerLog.println("-stderr "+sStderr);
+			writerLog.println("-pid "+sPid);
+			writerLog.println("-log "+sLog);
+			writerLog.println(sCall);
+			writerLog.println("-------------- STDOUT and STDERR----------------");
+			writerLog.close();
+
+			// Umleitung von STDOUT und STDERR dieses Scripts in das angegebene logfile
+			FileOutputStream logStream = new FileOutputStream(sLog);
+			PrintStream logPrintStream = new PrintStream(logStream);
+			System.setOut(logPrintStream);
+			System.setErr(logPrintStream);
 
 			// Aufruf taetigen
-			java.lang.Process sysproc = Runtime.getRuntime().exec(aufruf);
+			java.lang.Process sysproc = Runtime.getRuntime().exec(sCall);
 
 			// feststellen der Process-ID des laufenden JavaVM und in die PID-Datei schreiben
 			String pid = ManagementFactory.getRuntimeMXBean().getName();
@@ -63,20 +188,19 @@ public class Syscall {
 			String patt = "(\\d+)";
 			Pattern r = Pattern.compile(patt);
 			Matcher m = r.matcher(pid);
-			FileWriter fw_pid = new FileWriter(absdir_pid);
+			PrintWriter writerPid = new PrintWriter(sPid);
 			// wenn eine fuehrende zahl gefunden wird, wird diese als pid verwendet
 			if (m.find())
 			{
 				System.out.println("PID WIRD FESTGESTELLT ALS: "+m.group(1));
-				fw_pid.write(m.group(1));
+				writerPid.println(m.group(1));
 			}
 			// wenn keine fuehrende zahl gefunden wird, wird der gesamte string als pid verwendet
 			else
 			{
-				fw_pid.write(pid);
+				writerPid.println(pid);
 			}
-			fw_pid.flush();
-			fw_pid.close();
+			writerPid.close();
 			
 			// einfangen der stdout- und stderr
 			InputStream is_stdout = sysproc.getInputStream();
@@ -91,17 +215,13 @@ public class Syscall {
 			BufferedReader br_stderr = new BufferedReader(isr_stderr);
 			
 			// oeffnen der OutputStreams zu den Ausgabedateien
-			FileWriter fw_stdout = new FileWriter(absdir_stdout);
-			FileWriter fw_stderr = new FileWriter(absdir_stderr);
+			FileWriter fw_stdout = new FileWriter(sStdout);
+			FileWriter fw_stderr = new FileWriter(sStderr);
 			
 			// zeilenweise in die files schreiben
 			String line_out = new String();
 			String line_err = new String();
 
-			// DEBUG
-			System.out.println("CALL: "+aufruf);
-			System.out.println("SOUT: "+absdir_stdout);
-			System.out.println("SERR: "+absdir_stderr);
 
 			while ((((line_out = br_stdout.readLine()) != null) && ((line_err = br_stderr.readLine()) != null)) || ((line_err = br_stderr.readLine()) != null))
 			{
@@ -149,17 +269,24 @@ public class Syscall {
 			// Which makes sense. Java doesn't really know
 			// if your process failed. That must be determined
 			// from the exit value.
-			System.out.println("Exception happened - here's what I know: ");
+			System.out.println("IOException: Exception happened - here's what I know: ");
 			e.printStackTrace();
 		}
 		catch (InterruptedException e)
 		{
 			
 			// You need this for that waitFor() diddy.
-			System.out.println("Something got interrupted, "+
-				"I guess: "+e);
+			System.out.println("InterruptedException: Something got interrupted, I guess: ");
+			e.printStackTrace();
 		}
 
 	}
+	
+	private static void exiter()
+	{
+		System.out.println("try -help for help.");
+		System.exit(1);
+	}
+
 
 }
