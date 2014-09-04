e906c3fd168d62a640e627f2ddce8a73b1604be5
avoge <alexander.vogel@caegroup.de>
2014-09-04 17:25:39 +0200

    wartezeit zwischen start manager und refresh erhoeht 1sek -> 2sek

--- src/main/java/de/caegroup/pmodel/PmodelPartUi1.java
+++ src/main/java/de/caegroup/pmodel/PmodelPartUi1.java
@@ -654,8 +654,8 @@ public class PmodelPartUi1 extends ModelObject
 			
 			try
 			{
-				log("info", "waiting a second for process become available on disk");
-				Thread.sleep(1000);
+				log("info", "waiting 2 seconds for process become available on disk");
+				Thread.sleep(2000);
 			} catch (InterruptedException e)
 			{
 				// TODO Auto-generated catch block
