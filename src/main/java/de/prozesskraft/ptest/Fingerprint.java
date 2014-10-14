9cba2fa127888b3d01ba5f40fac97844bdc51fde
avoge <alexander.vogel@caegroup.de>
2014-10-14 18:40:32 +0200

    erste impl

---
+++ src/main/java/de/prozesskraft/ptest/Fingerprint.java
@@ -0,0 +1,252 @@
+package de.prozesskraft.ptest;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.util.ArrayList;
+import java.util.EnumSet;
+
+import javax.xml.bind.JAXBException;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.ini4j.Ini;
+import org.ini4j.InvalidFileFormatException;
+
+import de.caegroup.commons.*;
+import de.prozesskraft.ptest.*;
+
+import java.nio.file.attribute.*;
+
+public class Fingerprint
+{
+
+	/*----------------------------
+	  structure
+	----------------------------*/
+	static CommandLine commandline;
+	static String web = "www.prozesskraft.de";
+	static String author = "alexander.vogel@prozesskraft.de";
+	static String version = "[% version %]";
+	static String date = "[% date %]";
+	static Ini ini;
+	
+	/*----------------------------
+	  constructors
+	----------------------------*/
+//	public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException
+	public static void main(String[] args) throws org.apache.commons.cli.ParseException, IOException
+	{
+
+//		try
+//		{
+//			if (args.length != 3)
+//			{
+//				System.out.println("Please specify processdefinition file (xml) and an outputfilename");
+//			}
+//			
+//		}
+//		catch (ArrayIndexOutOfBoundsException e)
+//		{
+//			System.out.println("***ArrayIndexOutOfBoundsException: Please specify processdefinition.xml, openoffice_template.od*, newfile_for_processdefinitions.odt\n" + e.toString());
+//		}
+		
+		/*----------------------------
+		  get options from ini-file
+		----------------------------*/
+		File inifile = new java.io.File(WhereAmI.getInstallDirectoryAbsolutePath(Fingerprint.class) + "/" + "../etc/ptest-fingerprint.ini");
+
+		if (inifile.exists())
+		{
+			try
+			{
+				ini = new Ini(inifile);
+			}
+			catch (InvalidFileFormatException e1)
+			{
+			// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			catch (IOException e1)
+			{
+			// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		else
+		{
+			System.err.println("ini file does not exist: "+inifile.getAbsolutePath());
+			System.exit(1);
+		}
+		
+		/*----------------------------
+		  create boolean options
+		----------------------------*/
+		Option ohelp = new Option("help", "print this message");
+		Option ov = new Option("v", "prints version and build-date");
+		
+		/*----------------------------
+		  create argument options
+		----------------------------*/
+		Option opath = OptionBuilder.withArgName("PATH")
+				.hasArg()
+				.withDescription("[mandatory; default: .] the root path for the tree you want to make a fingerprint from.")
+//				.isRequired()
+				.create("path");
+		
+		Option ooutput = OptionBuilder.withArgName("FILE")
+				.hasArg()
+				.withDescription("[mandatory; default: fingerprint.xml] fingerprint file")
+//				.isRequired()
+				.create("output");
+		
+		/*----------------------------
+		  create options object
+		----------------------------*/
+		Options options = new Options();
+		
+		options.addOption( ohelp );
+		options.addOption( ov );
+		options.addOption( opath );
+		options.addOption( ooutput );
+		
+		/*----------------------------
+		  create the parser
+		----------------------------*/
+		CommandLineParser parser = new GnuParser();
+		try
+		{
+			// parse the command line arguments
+			commandline = parser.parse( options,  args );
+			
+		}
+		catch ( Exception exp )
+		{
+			// oops, something went wrong
+			System.err.println( "Parsing failed. Reason: "+ exp.getMessage());
+			exiter();
+		}
+		
+		/*----------------------------
+		  usage/help
+		----------------------------*/
+		if ( commandline.hasOption("help"))
+		{
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("fingerprint", options);
+			System.exit(0);
+		}
+		
+		else if ( commandline.hasOption("v"))
+		{
+			System.out.println("web:     "+web);
+			System.out.println("author: "+author);
+			System.out.println("version:"+version);
+			System.out.println("date:     "+date);
+			System.exit(0);
+		}
+
+		/*----------------------------
+		  ueberpruefen ob eine schlechte kombination von parametern angegeben wurde
+		----------------------------*/
+		String path = "";
+		String output = "";
+		if ( !( commandline.hasOption("path")) )
+		{
+			System.err.println("setting default for -path=.");
+			path = ".";
+		}
+		else
+		{
+			path = commandline.getOptionValue("path");
+		}
+
+		if ( !( commandline.hasOption("output")) )
+		{
+			System.err.println("setting default for -output=fingerprint.xml");
+			output = "fingerprint.xml";
+		}
+		else
+		{
+			output = commandline.getOptionValue("output");
+		}
+		
+//		if ( !( commandline.hasOption("output")) )
+//		{
+//			System.err.println("option -output is mandatory.");
+//			exiter();
+//		}
+
+		/*----------------------------
+		  die lizenz ueberpruefen und ggf abbrechen
+		----------------------------*/
+
+		// check for valid license
+		ArrayList<String> allPortAtHost = new ArrayList<String>();
+		allPortAtHost.add(ini.get("license-server", "license-server-1"));
+		allPortAtHost.add(ini.get("license-server", "license-server-2"));
+		allPortAtHost.add(ini.get("license-server", "license-server-3"));
+
+		MyLicense lic = new MyLicense(allPortAtHost, "1", "user-edition", "0.1");
+
+		// lizenz-logging ausgeben
+		for(String actLine : (ArrayList<String>) lic.getLog())
+		{
+			System.err.println(actLine);
+		}
+
+		// abbruch, wenn lizenz nicht valide
+		if (!lic.isValid())
+		{
+			System.exit(1);
+		}
+
+		/*----------------------------
+		  die eigentliche business logic
+		----------------------------*/
+		Batch batch = new Batch();
+		batch.setBasedir(path);
+		batch.setOutfilexml(output);
+
+		try
+		{
+			batch.genFingerprint();
+		}
+		catch (NullPointerException e)
+		{
+			System.err.println("file/dir does not exist "+path);
+			e.printStackTrace();
+			exiter();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			exiter();
+		}
+
+		batch.writeXml();
+		
+	}
+
+	private static void exiter()
+	{
+		System.out.println("try -help for help.");
+		System.exit(1);
+	}
+
+
+}
