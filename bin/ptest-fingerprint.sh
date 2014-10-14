15b9099584c6f0b00e2996967d47d862799c863e
avoge <alexander.vogel@caegroup.de>
2014-10-14 19:28:25 +0200

    korr

--- bin/ptest-fingerprint.sh
+++ bin/ptest-fingerprint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MAINCLASS="de.caegroup.process.Perlcode"
+MAINCLASS="de.prozesskraft.ptest.Fingerprint"
 BASEDIR=$(dirname $0)
 
 # wenn eine eigene javainstallation vorhanden ist, soll diese verwendet werden
