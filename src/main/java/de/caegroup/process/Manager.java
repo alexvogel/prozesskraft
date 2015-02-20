2e1a292627699e37b3beb6381fb7be9108426d2c
avoge <alexander.vogel@caegroup.de>
2015-02-20 18:02:43 +0100

    work

--- src/main/java/de/caegroup/process/Manager.java
+++ src/main/java/de/caegroup/process/Manager.java
@@ -391,7 +391,7 @@ public class Manager
 					}
 				}
 				// prozess laufen lassen
-				p3.doIt(ini.get("apps", "process-syscall"), ini.get("apps", "process-startinstance"));
+				p3.doIt(ini.get("apps", "process-syscall"), ini.get("apps", "process-startinstance"), ini.get("process", "domain-installation-directory"));
 	
 				// austeigen wenn status==error
 				if(p3.getStatus().equals("error"))
