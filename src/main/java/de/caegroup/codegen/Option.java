f19ac372d1fc400b06043c3aae032807894c7d9d
avoge <alexander.vogel@caegroup.de>
2015-04-08 18:57:54 +0200

    work

--- src/main/java/de/caegroup/codegen/Option.java
+++ src/main/java/de/caegroup/codegen/Option.java
@@ -40,7 +40,7 @@ implements Serializable, Cloneable
 	public ArrayList<String> getContent()
 	{
 		ArrayList<String> content = new ArrayList<String>();
-		content.add("				'" + this.name + "' => {'reihenfolge' =>'" + this.reihenfolge + "', 'minoccur' => '" + this.minoccur + "', 'maxoccur' => '" + this.maxoccur + "', 'definition' => '" + this.definition + "', 'check' => '" + this.check + "', 'default' => '" + this.def + "', 'allowIntegratedListIfMultiOption' => '" + this.allowIntegratedListIfMultiOption.toString() + "', 'text1' => '" + this.text1 + "', 'text2' => '" + this.text2 + "'},");
+		content.add("				'" + this.name + "' => {'reihenfolge' =>'" + this.reihenfolge + "', 'minoccur' => '" + this.minoccur + "', 'maxoccur' => '" + this.maxoccur + "', 'definition' => '" + this.definition + "', 'check' => '" + this.check + "', 'default' => '" + this.def + "', 'allowIntegratedListIfMultiOption' => '" + this.allowIntegratedListIfMultiOption.toString() + "', 'text1' => \"" + this.text1 + "\", 'text2' => \"" + this.text2 + "\"},");
 		return content;
 	}
 
