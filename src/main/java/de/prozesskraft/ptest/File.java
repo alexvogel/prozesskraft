b2278602746157b9c7eeda948ada728eda2eb953
avoge <alexander.vogel@caegroup.de>
2015-05-28 22:01:20 +0200

    undebug

--- src/main/java/de/prozesskraft/ptest/File.java
+++ src/main/java/de/prozesskraft/ptest/File.java
@@ -242,7 +242,7 @@ public class File {
 		// wenn ueber md5 gematched werden soll
 		if(this.respectMd5)
 		{
-			System.out.println(this.actRole+" file respectMD5: "+this.getPath());
+//			System.out.println(this.actRole+" file respectMD5: "+this.getPath());
 			if(this.flagMd5Matched)
 			{
 				return true;
@@ -251,7 +251,7 @@ public class File {
 		// sonst ueber alle anderen angaben matchen
 		else
 		{
-			System.out.println(this.actRole+"file DOES NOT respectMD5: "+this.getPath());
+//			System.out.println(this.actRole+"file DOES NOT respectMD5: "+this.getPath());
 			if(this.flagPathMatched && this.flagSizeMatched && this.flagOccuranceMatched && !this.flagFuzzyReference)	
 			{
 				return true;
