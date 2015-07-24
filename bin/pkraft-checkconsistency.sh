7680c1a1a46143a3692e146dc508e9114410abbc
avoge <alexander.vogel@caegroup.de>
2015-07-24 17:25:08 +0200

    ss

--- bin/pkraft-checkconsistency.sh
+++ bin/pkraft-checkconsistency.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MAINCLASS="de.caegroup.process.Checkconsistency"
+MAINCLASS="de.prozesskraft.pkraft.Checkconsistency"
 BASEDIR=$(dirname $0)
 
 # wenn eine eigene javainstallation vorhanden ist, soll diese verwendet werden
