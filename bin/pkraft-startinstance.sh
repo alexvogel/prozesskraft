a8bc421d210d36b75b22abce6a9385ab139968b2
avoge <alexander.vogel@caegroup.de>
2015-07-24 17:27:31 +0200

    sss

--- bin/pkraft-startinstance.sh
+++ bin/pkraft-startinstance.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MAINCLASS="de.caegroup.process.Startinstance"
+MAINCLASS="de.prozesskraft.pkraft.Startinstance"
 BASEDIR=$(dirname $0)
 
 # wenn eine eigene javainstallation vorhanden ist, soll diese verwendet werden
