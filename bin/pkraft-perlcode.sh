e1ce757d8dd92c27b4c16160dfbbd8210b69757c
avoge <alexander.vogel@caegroup.de>
2015-07-24 17:27:13 +0200

    sss

--- bin/pkraft-perlcode.sh
+++ bin/pkraft-perlcode.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MAINCLASS="de.caegroup.process.Perlcode"
+MAINCLASS="de.prozesskraft.pkraft.Perlcode"
 BASEDIR=$(dirname $0)
 
 # wenn eine eigene javainstallation vorhanden ist, soll diese verwendet werden
