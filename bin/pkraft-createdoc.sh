5a2177265a71d385ea97a5768c2527b67f8c74ed
avoge <alexander.vogel@caegroup.de>
2015-07-24 17:26:00 +0200

    sss

--- bin/pkraft-createdoc.sh
+++ bin/pkraft-createdoc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MAINCLASS="de.caegroup.process.Createdoc"
+MAINCLASS="de.prozesskraft.pkraft.Createdoc"
 BASEDIR=$(dirname $0)
 
 # wenn eine eigene javainstallation vorhanden ist, soll diese verwendet werden
