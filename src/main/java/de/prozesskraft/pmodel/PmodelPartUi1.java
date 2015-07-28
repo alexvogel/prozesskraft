a11d9da67303239fc9f97035149069c6f1821dbe
avoge <alexander.vogel@caegroup.de>
2015-07-28 19:42:54 +0200

    www

--- src/main/java/de/prozesskraft/pmodel/PmodelPartUi1.java
+++ src/main/java/de/prozesskraft/pmodel/PmodelPartUi1.java
@@ -150,7 +150,7 @@ public class PmodelPartUi1 extends ModelObject
 		loadIni();
 		checkLicense();
 		createControls(composite);
-//		this.einstellungen.getProcess().setStepRanks();
+		this.einstellungen.getProcess().setStepRanks();
 		applet = new PmodelViewPage(einstellungen);
 	}
 
@@ -163,7 +163,7 @@ public class PmodelPartUi1 extends ModelObject
 		setIni();
 		loadIni();
 		checkLicense();
-//		this.einstellungen.getProcess().setStepRanks();
+		this.einstellungen.getProcess().setStepRanks();
 		applet = new PmodelViewPage(einstellungen);
 		createControls(composite);
 	}
