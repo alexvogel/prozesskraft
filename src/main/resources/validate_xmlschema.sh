c1f343b389f6d63985fd8cad2f0bd04f90b2816d
avoge <alexander.vogel@caegroup.de>
2015-08-21 13:22:33 +0200

    sss

--- src/main/resources/validate_xmlschema.sh
+++ src/main/resources/validate_xmlschema.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-xmllint --schema process.xsd ../../test/resources/definitions/Exterior/beulen/0.1.0/process.xml
+#xmllint --schema process.xsd ../../test/resources/definitions/Exterior/beulen/0.1.0/process.xml
+xmllint --schema process.xsd ../../../../../workspace_caegroup/processes/definitions/Exterior/multibeulen+/2.0.0/process.xml
