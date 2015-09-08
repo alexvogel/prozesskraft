468813a1c9be18336770a987dbf8363b6a0e2ed8
avoge <alexander.vogel@caegroup.de>
2015-09-08 17:31:48 +0200

    initialcommit

---
+++ src/main/java/de/prozesskraft/pkraft/Clone.java
@@ -0,0 +1,179 @@
+package de.prozesskraft.pkraft;
+
+//import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+import javax.xml.bind.JAXBException;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.ini4j.Ini;
+import org.ini4j.InvalidFileFormatException;
+
+import de.prozesskraft.commons.MyLicense;
+import de.prozesskraft.commons.WhereAmI;
+
+public class Clone
+{
+
+	/*----------------------------
+	  structure
+	----------------------------*/
+	static CommandLine commandline;
+	static Ini ini;
+	
+	/*----------------------------
+	  constructors
+	----------------------------*/
+	public static void main(String[] args) throws org.apache.commons.cli.ParseException, IOException
+	{
+
+		/*----------------------------
+		  get options from ini-file
+		----------------------------*/
+		java.io.File inifile = new java.io.File(WhereAmI.getInstallDirectoryAbsolutePath(Clone.class) + "/" + "../etc/pkraft-startinstance.ini");
+
+		if (inifile.exists())
+		{
+			try
+			{
+				ini = new Ini(inifile);
+			}
+			catch (InvalidFileFormatException e1)
+			{
+			// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			catch (IOException e1)
+			{
+			// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		else
+		{
+			System.err.println("ini file does not exist: "+inifile.getAbsolutePath());
+			System.exit(1);
+		}
+		
+		/*----------------------------
+		  create boolean options
+		----------------------------*/
+		Option ohelp = new Option("help", "print this message");
+		Option ov = new Option("v", "prints version and build-date");
+		
+		/*----------------------------
+		  create argument options
+		----------------------------*/
+		Option oinstance = OptionBuilder.withArgName("File")
+				.hasArg()
+				.withDescription("[mandatory] process you want to clone.")
+//				.isRequired()
+				.create("instance");
+		
+
+		/*----------------------------
+		  create options object
+		----------------------------*/
+		Options options = new Options();
+		
+		options.addOption( ohelp );
+		options.addOption( ov );
+		options.addOption( oinstance );
+		
+		/*----------------------------
+		  create the parser
+		----------------------------*/
+		CommandLineParser parser = new GnuParser();
+		// parse the command line arguments
+		commandline = parser.parse( options,  args );
+		
+		
+		/*----------------------------
+		  usage/help
+		----------------------------*/
+		if ( commandline.hasOption("help"))
+		{
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("startinstance", options);
+			System.exit(0);
+		}
+
+		if ( commandline.hasOption("v"))
+		{
+			System.out.println("author:  alexander.vogel@caegroup.de");
+			System.out.println("version: [% version %]");
+			System.out.println("date:    [% date %]");
+			System.exit(0);
+		}
+		/*----------------------------
+		  ueberpruefen ob eine schlechte kombination von parametern angegeben wurde
+		----------------------------*/
+		if ( !( commandline.hasOption("instance")))
+		{
+			System.err.println("option -instance is mandatory");
+			exiter();
+		}
+
+		/*----------------------------
+		  die lizenz ueberpruefen und ggf abbrechen
+		----------------------------*/
+
+		// check for valid license
+		ArrayList<String> allPortAtHost = new ArrayList<String>();
+		allPortAtHost.add(ini.get("license-server", "license-server-1"));
+		allPortAtHost.add(ini.get("license-server", "license-server-2"));
+		allPortAtHost.add(ini.get("license-server", "license-server-3"));
+
+		MyLicense lic = new MyLicense(allPortAtHost, "1", "user-edition", "0.1");
+
+		// lizenz-logging ausgeben
+		for(String actLine : (ArrayList<String>) lic.getLog())
+		{
+			System.err.println(actLine);
+		}
+
+		// abbruch, wenn lizenz nicht valide
+		if (!lic.isValid())
+		{
+			System.exit(1);
+		}
+		
+		/*----------------------------
+		  die eigentliche business logic
+		----------------------------*/
+		String pathToInstance = commandline.getOptionValue("instance");
+		java.io.File fileInstance = new java.io.File(pathToInstance);
+		
+		// wenn es nicht vorhanden ist, dann mit fehlermeldung abbrechen
+		if(!fileInstance.exists())
+		{
+			System.err.println("instance file does not exist.");
+			exiter();
+		}
+		
+		Process p1 = new Process();
+		p1.setInfilebinary(pathToInstance);
+		
+		Process p2 = p1.readBinary();
+		
+		Process p3 = p2.cloneWithData();
+
+	}
+	
+	private static void exiter()
+	{
+		System.out.println("try -help for help.");
+		System.exit(1);
+	}
+
+
+}
