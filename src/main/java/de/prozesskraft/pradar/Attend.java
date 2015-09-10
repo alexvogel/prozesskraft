bcb808a5a59de0d45b8905a3e3f8c2c84b478023
avoge <alexander.vogel@caegroup.de>
2015-09-10 19:06:19 +0200

    sss

--- src/main/java/de/prozesskraft/pradar/Attend.java
+++ src/main/java/de/prozesskraft/pradar/Attend.java
@@ -165,7 +165,8 @@ public class Attend
 		// instanz einlesen
 		Process process = new Process();
 		process.setInfilebinary(fileProcessBinary.getAbsolutePath());
-
+		process.readBinary();
+		
 		// ein pradar entity erstellen
 		Entity entity = new Entity();
 
