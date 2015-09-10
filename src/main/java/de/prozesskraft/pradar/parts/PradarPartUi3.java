b1970956d735e33186fea1e8fa404280f88e6881
avoge <alexander.vogel@caegroup.de>
2015-09-10 13:53:38 +0200

    ssss

--- src/main/java/de/prozesskraft/pradar/parts/PradarPartUi3.java
+++ src/main/java/de/prozesskraft/pradar/parts/PradarPartUi3.java
@@ -356,6 +356,14 @@ public class PradarPartUi3 extends ModelObject
 		button_delete.setText("delete");
 		button_delete.setToolTipText("deletes a finished (already checked out) process instance from database. includes an implicit 'clean'");
 		button_delete.addSelectionListener(listener_delete_button);
+		if ( (einstellungen.entitySelected != null) && ( einstellungen.entitySelected.getParentid()).equals("0") )
+		{
+			button_delete.setEnabled(true);
+		}
+		else
+		{
+			button_delete.setEnabled(false);
+		}
 		
 		// Group visual
 		Group grpVisual = new Group(composite_11, SWT.NONE);
