57b2224dcff84f50ef882d3c57f4c093d27913fc
avoge <alexander.vogel@caegroup.de>
2015-09-23 13:10:02 +0200

    sss

--- src/main/java/de/prozesskraft/pkraft/Subprocess.java
+++ src/main/java/de/prozesskraft/pkraft/Subprocess.java
@@ -450,6 +450,8 @@ implements Serializable
 		try
 		{
 			newProcess.setInfilexml(processDef.getCanonicalPath());
+			newProcess.setInfilebinary(this.getParent().getAbsdir() + "/process.pmb");
+			newProcess.setOutfilebinary(this.getParent().getAbsdir() + "/process.pmb");
 		}
 		catch (IOException e1)
 		{
