123e87943cb41fab658d63271ac4ae2d433ac680
avoge <alexander.vogel@caegroup.de>
2015-10-08 13:45:10 +0200

    sss

--- src/main/java/de/prozesskraft/pkraft/Subprocess.java
+++ src/main/java/de/prozesskraft/pkraft/Subprocess.java
@@ -170,8 +170,9 @@ implements Serializable
 				log("info", "subprocess not running anymore. pid="+pid);
 				
 				// den status des subprocesses feststellen und this auf den gleichen setzen
-				this.getProcess().readBinary();
-				this.setStatus(this.getProcess().getStatus());
+				Process processReread = this.getProcess().readBinary();
+				this.setProcess(processReread);
+				this.setStatus(processReread.getStatus());
 				log("info", "setting status to " + this.getStatus());
 			}
 		}
