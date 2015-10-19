2f813a192f04ed14b25f47bfa52b48dcc8d2ec39
avoge <alexander.vogel@caegroup.de>
2015-10-19 18:06:27 +0200

    debug

--- src/main/java/de/prozesskraft/pkraft/Work.java
+++ src/main/java/de/prozesskraft/pkraft/Work.java
@@ -538,7 +538,7 @@ implements Serializable
 			double actLoadAverage = ManagementFactory.getOperatingSystemMXBean().getSystemLoadAverage();
 			log("debug", "the present load average is " + actLoadAverage);
 			this.getParent().getParent().memorizeLoadAverage();
-			log("debug", "all the load averages till now are " + this.getParent().getParent().getTimeSerieLoadAverage().sprint());
+//			log("debug", "all the load averages till now are " + this.getParent().getParent().getTimeSerieLoadAverage().sprint());
 
 			// 3) das load average unterhalb eines definierten wertes
 			if( (this.getParent().getParent().getStepStartLoadAverageBelow() != null) && (minutesSinceLastStepStart < this.getParent().getParent().getStepStartLoadAverageBelow()) )
