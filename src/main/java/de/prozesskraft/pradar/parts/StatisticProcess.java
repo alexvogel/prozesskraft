4175d0ff12699a5e25ad21dd1e50fce6e6e17412
avoge <alexander.vogel@caegroup.de>
2015-11-03 16:38:01 +0100

    gannt uebrwschrift festgelegt

--- src/main/java/de/prozesskraft/pradar/parts/StatisticProcess.java
+++ src/main/java/de/prozesskraft/pradar/parts/StatisticProcess.java
@@ -46,6 +46,7 @@ import org.eclipse.swt.layout.FormLayout;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.title.LegendTitle;
 import org.jfree.data.category.IntervalCategoryDataset;
 import org.jfree.data.gantt.Task;
 import org.jfree.data.gantt.TaskSeries;
@@ -146,6 +147,7 @@ public class StatisticProcess
 
 		final IntervalCategoryDataset dataset = createDatasetGantt();  
 		final JFreeChart chart = createChart(dataset);  
+		chart.setTitle("Process History");
   
 		Composite embeddedComposite = new Composite(composite, SWT.EMBEDDED);
 		embeddedComposite.setLayoutData(new GridData(GridData.FILL_VERTICAL | GridData.FILL_HORIZONTAL));
@@ -261,85 +263,7 @@ public class StatisticProcess
     	}
         return collection;
 
-        
-        
-//        final TaskSeries s1 = new TaskSeries("Scheduled");
-//        s1.add(new Task("Write Proposal",
-//               new SimpleTimePeriod(date(1, Calendar.APRIL, 2001),
-//                                    date(5, Calendar.APRIL, 2001))));
-//        s1.add(new Task("Obtain Approval",
-//               new SimpleTimePeriod(date(9, Calendar.APRIL, 2001),
-//                                    date(9, Calendar.APRIL, 2001))));
-//        s1.add(new Task("Requirements Analysis",
-//               new SimpleTimePeriod(date(10, Calendar.APRIL, 2001),
-//                                    date(5, Calendar.MAY, 2001))));
-//        s1.add(new Task("Design Phase",
-//               new SimpleTimePeriod(date(6, Calendar.MAY, 2001),
-//                                    date(30, Calendar.MAY, 2001))));
-//        s1.add(new Task("Design Signoff",
-//               new SimpleTimePeriod(date(2, Calendar.JUNE, 2001),
-//                                    date(2, Calendar.JUNE, 2001))));
-//        s1.add(new Task("Alpha Implementation",
-//               new SimpleTimePeriod(date(3, Calendar.JUNE, 2001),
-//                                    date(31, Calendar.JULY, 2001))));
-//        s1.add(new Task("Design Review",
-//               new SimpleTimePeriod(date(1, Calendar.AUGUST, 2001),
-//                                    date(8, Calendar.AUGUST, 2001))));
-//        s1.add(new Task("Revised Design Signoff",
-//               new SimpleTimePeriod(date(10, Calendar.AUGUST, 2001),
-//                                    date(10, Calendar.AUGUST, 2001))));
-//        s1.add(new Task("Beta Implementation",
-//               new SimpleTimePeriod(date(12, Calendar.AUGUST, 2001),
-//                                    date(12, Calendar.SEPTEMBER, 2001))));
-//        s1.add(new Task("Testing",
-//               new SimpleTimePeriod(date(13, Calendar.SEPTEMBER, 2001),
-//                                    date(31, Calendar.OCTOBER, 2001))));
-//        s1.add(new Task("Final Implementation",
-//               new SimpleTimePeriod(date(1, Calendar.NOVEMBER, 2001),
-//                                    date(15, Calendar.NOVEMBER, 2001))));
-//        s1.add(new Task("Signoff",
-//               new SimpleTimePeriod(date(28, Calendar.NOVEMBER, 2001),
-//                                    date(30, Calendar.NOVEMBER, 2001))));
-//
-//        final TaskSeries s2 = new TaskSeries("Actual");
-//        s2.add(new Task("Write Proposal",
-//               new SimpleTimePeriod(date(1, Calendar.APRIL, 2001),
-//                                    date(5, Calendar.APRIL, 2001))));
-//        s2.add(new Task("Obtain Approval",
-//               new SimpleTimePeriod(date(9, Calendar.APRIL, 2001),
-//                                    date(9, Calendar.APRIL, 2001))));
-//        s2.add(new Task("Requirements Analysis",
-//               new SimpleTimePeriod(date(10, Calendar.APRIL, 2001),
-//                                    date(15, Calendar.MAY, 2001))));
-//        s2.add(new Task("Design Phase",
-//               new SimpleTimePeriod(date(15, Calendar.MAY, 2001),
-//                                    date(17, Calendar.JUNE, 2001))));
-//        s2.add(new Task("Design Signoff",
-//               new SimpleTimePeriod(date(30, Calendar.JUNE, 2001),
-//                                    date(30, Calendar.JUNE, 2001))));
-//        s2.add(new Task("Alpha Implementation",
-//               new SimpleTimePeriod(date(1, Calendar.JULY, 2001),
-//                                    date(12, Calendar.SEPTEMBER, 2001))));
-//        s2.add(new Task("Design Review",
-//               new SimpleTimePeriod(date(12, Calendar.SEPTEMBER, 2001),
-//                                    date(22, Calendar.SEPTEMBER, 2001))));
-//        s2.add(new Task("Revised Design Signoff",
-//               new SimpleTimePeriod(date(25, Calendar.SEPTEMBER, 2001),
-//                                    date(27, Calendar.SEPTEMBER, 2001))));
-//        s2.add(new Task("Beta Implementation",
-//               new SimpleTimePeriod(date(27, Calendar.SEPTEMBER, 2001),
-//                                    date(30, Calendar.OCTOBER, 2001))));
-//        s2.add(new Task("Testing",
-//               new SimpleTimePeriod(date(31, Calendar.OCTOBER, 2001),
-//                                    date(17, Calendar.NOVEMBER, 2001))));
-//        s2.add(new Task("Final Implementation",
-//               new SimpleTimePeriod(date(18, Calendar.NOVEMBER, 2001),
-//                                    date(5, Calendar.DECEMBER, 2001))));
-//        s2.add(new Task("Signoff",
-//               new SimpleTimePeriod(date(10, Calendar.DECEMBER, 2001),
-//                                    date(11, Calendar.DECEMBER, 2001))));
-
-    }
+     }
 
     /**
      * Utility method for creating <code>Date</code> objects.
