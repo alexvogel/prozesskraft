50d0e31bcf04d4d4d65910deca1a0d01aace994e
avoge <alexander.vogel@caegroup.de>
2016-09-03 13:51:01 +0200

    opt

--- src/main/java/de/prozesskraft/codegen/BHelp.java
+++ src/main/java/de/prozesskraft/codegen/BHelp.java
@@ -82,7 +82,7 @@ implements Serializable, Cloneable
 			content.add("			{");
 			content.add("				$helptext .= \", default: \" . ${$OPTHELP{$_}}{'textfordefault'} . \")\";");
 			content.add("			}");
-			content.add("			elsif (exists ${$OPTHELP{$_}}{'default'})");
+			content.add("			elsif ((exists ${$OPTHELP{$_}}{'default'}) && (${$OPTHELP{$_}}{'default'} != undef))");
 			content.add("			{");
 			content.add("				$helptext .= \", default: \" . ${$OPTHELP{$_}}{'default'} . \")\";");
 			content.add("			}");
